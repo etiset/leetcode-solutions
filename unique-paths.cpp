5024152417faaab9cc679572ef4689489d5a01ea
tnxcyrz <txnyczr@gmail.com>
2024-10-15 02:19:33 -0400

    Add solution for unique-paths.

---
+++ unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, 1);
+        vector<int> curr(n);
+
+        for(int i = 1; i < m; i++){
+            for(int j = 0; j < n; j++){
+                curr[j] = prev[j];
+
+                if(j > 0){
+                    curr[j] += curr[j-1];
+                }
+            }
+
+            swap(prev, curr);
+        }
+
+        return prev[n-1];
+    }
+};
