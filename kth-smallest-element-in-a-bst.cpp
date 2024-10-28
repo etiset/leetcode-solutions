c9c500ba3673a4e127ee3e13edbbddf536cc92f7
tnxcyrz <txnyczr@gmail.com>
2024-10-27 23:10:59 -0400

    Add solution for kth-smallest-element-in-a-bst.

---
+++ kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int traverseTree(TreeNode* root, int &k){
+        if(root -> left != NULL){
+            int res = traverseTree(root -> left, k);
+
+            if(k == 0){
+                return res;
+            }
+        }
+
+        k--;
+
+        if(k == 0){
+            return root -> val;
+        }
+
+        if(root -> right != NULL){
+            int res = traverseTree(root -> right, k);
+
+            if(k == 0){
+                return res;
+            }
+        }
+
+        return -1;
+    }
+
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        return traverseTree(root, k);
+    }
+};
