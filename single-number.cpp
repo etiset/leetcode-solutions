47af9f6978a1a63b6a631de14a6e72f9c4f8ea06
tnxcyrz <txnyczr@gmail.com>
2025-06-19 23:49:09 -0400

    Add solution for single-number.

---
+++ single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+
+        for(int x: nums){
+            res ^= x;
+        }
+
+        return res;
+    }
+};
