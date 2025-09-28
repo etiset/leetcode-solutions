ad048386ba900580db2ba8e9819abe7ad5c186fa
tnxcyrz <txnyczr@gmail.com>
2025-09-28 14:04:45 -0400

    Add solution for valid-parentheses.

---
+++ valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> ops = {'(', '{', '['};
+        vector<char> cls = {')', '}', ']'};
+
+        stack<char> st;
+
+        for(char c: s){
+            auto it = find(cls.begin(), cls.end(), c);
+
+            if(it == cls.end()){
+                st.push(c);
+                continue;
+            }
+
+            int idx = it - cls.begin();
+
+            if(st.empty() || st.top() != ops[idx]){
+                return false;
+            }
+
+            st.pop();
+        }
+
+        return st.empty();
+    }
+};
