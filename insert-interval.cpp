b3b782282875b703b7e60d76b65b626e85fddf67
tnxcyrz <txnyczr@gmail.com>
2026-02-26 21:56:16 -0500

    Add solution for insert-interval.

---
+++ insert-interval.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+
+        int idx = 0;
+
+        while(idx < intervals.size() && intervals[idx][1] < newInterval[0]){
+            res.push_back(intervals[idx++]);
+        }
+
+        while(idx < intervals.size() && newInterval[1] >= intervals[idx][0]){
+            newInterval[0] = min(newInterval[0], intervals[idx][0]);
+            newInterval[1] = max(newInterval[1], intervals[idx][1]);
+            
+            idx++;
+        }
+
+        res.push_back(newInterval);
+
+        while(idx < intervals.size()){
+            res.push_back(intervals[idx++]);
+        }
+
+        return res;
+    }
+};
