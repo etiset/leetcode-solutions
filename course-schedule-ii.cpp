645dcf819144b6f27742adcb1500978f3f0d394b
tnxcyrz <txnyczr@gmail.com>
2026-03-12 23:53:34 -0400

    Add solution for course-schedule-ii.

---
+++ course-schedule-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> deg(numCourses);
+        vector<vector<int>> pars(numCourses);
+
+        for(vector<int>& it: prerequisites){
+            deg[it[0]]++;
+            pars[it[1]].push_back(it[0]);
+        }
+
+        queue<int> qu;
+
+        for(int i = 0; i < numCourses; i++){
+            if(deg[i] == 0){
+                qu.push(i);
+            }
+        }
+
+        vector<int> res;
+
+        while(!qu.empty()){
+            int u = qu.front();
+            qu.pop();
+
+            res.push_back(u);
+
+            for(int v: pars[u]){
+                deg[v]--;
+
+                if(deg[v] == 0){
+                    qu.push(v);
+                }
+            }
+        }
+
+        if(res.size() < numCourses){
+            res.clear();
+        }
+
+        return res;
+    }
+};
