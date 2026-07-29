9849c333c9c7efc38dcfeba730f7152c5b058ab2
tnxcyrz <txnyczr@gmail.com>
2026-07-29 10:59:08 -0400

    Add solution for jump-game.

---
+++ jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int can_reach = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(i > can_reach){
+                return false;
+            }
+
+            can_reach = max(can_reach, i + nums[i]);
+        }
+
+        return true;
+    }
+};
